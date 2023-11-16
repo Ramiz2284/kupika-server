f8666791e85bec5c9dd9c9c45b7b44376d6a01ce
Ramiz2284 <sultan22.84@mail.ru>
2023-11-16 18:45:26 +0300

    chatbot asistent add

--- controllers/chatBotController.js
+++ controllers/chatBotController.js
@@ -1,20 +1,50 @@
 require('dotenv').config()
-
-const OpenAI = require('openai')
+const { OpenAI } = require('openai')
 
 const openai = new OpenAI({
 	apiKey: process.env.OPENAI_API_KEY,
 })
 
+const assistantId = process.env.YOUR_ASSISTANT_ID // ID ассистента
+
 exports.question = async (req, res) => {
 	try {
-		const chatCompletion = await openai.chat.completions.create({
-			messages: [{ role: 'user', content: req.body.message }],
-			model: 'gpt-3.5-turbo', // Выберите нужную модель
+		const question = req.body.message // Убедитесь, что вопрос передается в теле запроса
+
+		// Создание потока (thread)
+		const thread = await openai.beta.threads.create()
+
+		// Добавление сообщения в поток
+		await openai.beta.threads.messages.create(thread.id, {
+			role: 'user',
+			content: question,
 		})
 
-		// Отправьте ответ пользователю
-		res.json({ reply: chatCompletion.choices[0].message.content.trim() })
+		// Запуск ассистента
+		const run = await openai.beta.threads.runs.create(thread.id, {
+			assistant_id: assistantId, // Использование существующего ID ассистента
+		})
+
+		// Ожидание завершения выполнения ассистента
+		let runStatus
+		do {
+			const runStatusResponse = await openai.beta.threads.runs.retrieve(
+				thread.id,
+				run.id
+			)
+			runStatus = runStatusResponse.status
+			await new Promise(resolve => setTimeout(resolve, 1000)) // Пауза в 1 секунду
+		} while (runStatus === 'in_progress')
+
+		// Получение ответов ассистента
+		const messages = await openai.beta.threads.messages.list(thread.id)
+
+		// Формирование массива ответов ассистента
+		const assistantReplies = messages.data
+			.filter(msg => msg.role === 'assistant')
+			.map(msg => msg.content.map(c => (c.text ? c.text.value : '')).join(''))
+		// Отправка ответов пользователю
+		res.json({ replies: assistantReplies })
 	} catch (error) {
 		console.error('Error:', error)
 		res.status(500).send('Internal Server Error')
