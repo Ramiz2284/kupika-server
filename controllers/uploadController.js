0ae02b99914b29ab2405d33425f5f38a337d4e13
Ramiz2284 <sultan22.84@mail.ru>
2023-11-21 20:11:06 +0300

    fix uploadController

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -1,31 +1,40 @@
 const multer = require('multer')
 const sharp = require('sharp')
 const path = require('path')
-const fs = require('fs').promises // Измените на использование промисов
+const fs = require('fs').promises // Используйте fs.promises для асинхронных операций
 
-const storage = multer.memoryStorage() // Измените хранение на memoryStorage
+// Настройка хранилища на диске
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		// Укажите путь к папке, где будут храниться загруженные файлы
+		cb(null, 'uploads/')
+	},
+	filename: function (req, file, cb) {
+		// Создание уникального имени файла
+		const cleanName = file.originalname.replace(/[^a-zA-Z0-9.]+/g, '-')
+		const filename = Date.now() + '-' + cleanName
+		cb(null, filename)
+	},
+})
 
 const upload = multer({ storage: storage })
 
 const uploadRoute = async (req, res) => {
 	try {
-		// Доступ к файлу через req.file.buffer
-		const buffer = await sharp(req.file.buffer)
-			.toFormat('jpeg') // Конвертировать в jpeg
-			.jpeg({ quality: 60 }) // Установите качество сжатого изображения
+		// Загрузка и обработка файла
+		const filepath = path.join('uploads/', req.file.filename) // Получаем путь к загруженному файлу
+
+		// Преобразование изображения с помощью sharp и сохранение его на диск
+		const buffer = await sharp(filepath)
+			.toFormat('jpeg')
+			.jpeg({ quality: 60 })
 			.toBuffer()
 
-		// Сохранение обработанного изображения на диск
-		const cleanName = req.file.originalname.replace(/[^a-zA-Z0-9.]+/g, '-')
-		const filename = Date.now() + '-' + cleanName
-		const filepath = path.join('uploads/', filename)
+		await fs.writeFile(filepath, buffer) // Асинхронное сохранение обработанного файла
 
-		// Асинхронная запись файла на диск
-		await fs.writeFile(filepath, buffer)
-
-		res.json({ photoUrl: `/uploads/${filename}` })
+		res.json({ photoUrl: `/uploads/${req.file.filename}` })
 	} catch (error) {
-		console.error('Error processing image', error) // Добавьте логирование ошибки
+		console.error('Error processing image', error)
 		res.status(500).json({ message: 'Error processing image', error })
 	}
 }
